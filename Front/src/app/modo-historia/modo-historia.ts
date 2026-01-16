02551cfd9acc6d8297946120799c10eb0bf9d3f8
Haziel <haziel030306@gmail.com>
2026-01-16 10:41:04 +0100

    html simple convertido a componentes Angular

---
+++ Front/src/app/modo-historia/modo-historia.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modo-historia',
+  imports: [],
+  templateUrl: './modo-historia.html',
+  styleUrl: './modo-historia.css',
+})
+export class ModoHistoria {
+
+}
---
+++ Front/src/app/soporte/soporte.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soporte',
+  imports: [],
+  templateUrl: './soporte.html',
+  styleUrl: './soporte.css',
+})
+export class Soporte {
+
+}
